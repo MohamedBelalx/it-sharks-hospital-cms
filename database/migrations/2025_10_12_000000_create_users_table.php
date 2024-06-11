18f5faed18328a9be9fb17d850fff46368911668
Mohamed Belal <hero.armna@gmail.com>
2024-06-11 18:12:25 +0200

    add department and users talbes

--- database/migrations/2025_10_12_000000_create_users_table.php
+++ database/migrations/2025_10_12_000000_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['doctor', 'patient', 'nurse', 'pharmacy', 'admin']);
+            $table->enum('gender', ['male', 'female']);
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('department_id')->nullable();
+            $table->foreign('department_id')->references('id')->on('department');
             $table->rememberToken();
             $table->timestamps();
         });
